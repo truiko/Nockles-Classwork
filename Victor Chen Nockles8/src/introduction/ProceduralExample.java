3cd98476eb5cf0ad7af1f67c5e066fc26bed59ef
truiko <hotfire619@gmail.com>
2016-09-15 13:52:54 -0400

    Update ProceduralExample.java

--- Victor Chen Nockles8/src/introduction/ProceduralExample.java
+++ Victor Chen Nockles8/src/introduction/ProceduralExample.java
@@ -3,6 +3,7 @@ package introduction;
 public class ProceduralExample {
 public static void main(String[] args){
 	String[] names = {"Jillian","Joseph","Jordan"};
-	System.out.println("Hi, my name is" + names[0] + "." );
+	String[] years = {"Sophomore","Junior","Senior"};
+	System.out.println("Hi, my name is" + names[0] + " and I am a" + years[0] + "." );
 }
 }
