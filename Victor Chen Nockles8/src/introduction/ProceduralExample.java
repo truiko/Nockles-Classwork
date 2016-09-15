292932186d9d10b32aaca7ee83d9f568ee0a7b17
Student 8 <Student 8@10.8.33.152>
2016-09-15 13:42:58 -0400

    Jillian says her name

---
+++ Victor Chen Nockles8/src/introduction/ProceduralExample.java
@@ -0,0 +1,8 @@
+package introduction;
+
+public class ProceduralExample {
+public static void main(String[] args){
+	String[] names = {"Jillian","Joseph","Jordan"};
+	System.out.println("Hi, my name is" + names[0] + "." );
+}
+}
