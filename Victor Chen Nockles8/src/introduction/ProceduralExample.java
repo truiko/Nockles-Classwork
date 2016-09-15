aac00c86fd7351fcfbe29a9b613ba3eb5797fc68
Student 8 <Student 8@10.8.33.152>
2016-09-15 14:05:05 -0400

    fix that bug. check at home. don't break school computer

--- Victor Chen Nockles8/src/introduction/ProceduralExample.java
+++ Victor Chen Nockles8/src/introduction/ProceduralExample.java
@@ -4,6 +4,8 @@ public class ProceduralExample {
 public static void main(String[] args){
 	String[] names = {"Jillian","Joseph","Jordan"};
 	String[] years = {"Sophomore","Junior","Senior"};
-	System.out.println("Hi, my name is" + names[0] + " and I am a" + years[0] + "." );
+	System.out.println("Hi, my name is " + names[0] + " and I am a " + years[0] + "." );
+	System.out.println("Hi, my name is " + names[1] + " and I am a " + years[1] + "." );
+	System.out.println("Hi, my name is " + names[2] + " and I am a " + years[2] + "." );
 }
 }
