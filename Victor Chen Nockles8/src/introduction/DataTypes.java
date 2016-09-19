2160c451a8fddfa6fa07441312705a3d9be57a44
hotfi <hotfi@192.168.1.2>
2016-09-19 18:48:35 -0400

    finished orcmath 0.3

--- Victor Chen Nockles8/src/introduction/DataTypes.java
+++ Victor Chen Nockles8/src/introduction/DataTypes.java
@@ -3,14 +3,9 @@ package introduction;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		double d1 = 4.64; 
-		double d2 = 2.0;
-		double d3 = 2.64;
-
-		System.out.println("d1 : " + d1); 
-		System.out.println("d2 : " + d2); 
-		System.out.println("d3 : " + d3);  
-		System.out.println("d1 - d2 : " + (d1 - d2));
+	    int m = 22;
+	    int n = 7;
+	    System.out.println("m/n is "+((double)m/n));
 	}
 
 }
