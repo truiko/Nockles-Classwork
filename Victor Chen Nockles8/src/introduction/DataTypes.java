05b45e6ed31c8885acda2529f6a9c6e34bf7b6f5
Student 8 <Student 8@10.8.33.152>
2016-09-19 14:02:09 -0400

    finish test precision, string exercises, and advanced on orcmath .3

---
+++ Victor Chen Nockles8/src/introduction/DataTypes.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		double d1 = 4.64; 
+		double d2 = 2.0;
+		double d3 = 2.64;
+
+		System.out.println("d1 : " + d1); 
+		System.out.println("d2 : " + d2); 
+		System.out.println("d3 : " + d3);  
+		System.out.println("d1 - d2 : " + (d1 - d2));
+	}
+
+}
