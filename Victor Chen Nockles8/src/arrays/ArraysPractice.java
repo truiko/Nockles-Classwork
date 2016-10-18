6fbb3cf1dcc369a7bfcc32ce85be2cf4d918046e
Student 8 <Student 8@10.8.33.152>
2016-10-18 14:04:07 -0400

    wasnt paying attention

--- Victor Chen Nockles8/src/arrays/ArraysPractice.java
+++ Victor Chen Nockles8/src/arrays/ArraysPractice.java
@@ -7,18 +7,82 @@ public class ArraysPractice {
 	public static void main(String[] args) {
 		// how do you time a process
 		long currentTime = System.currentTimeMillis();
+		int[] fiftyNumbers = new int[50];
+		populate(fiftyNumbers);
+		print(fiftyNumbers);
+		randomize(fiftyNumbers, 50);
+		print(fiftyNumbers);
+		rollDice(fiftyNumbers, 4);
+		print(fiftyNumbers);
+		// count each die roll and provide a percentage
+		countResult(fiftyNumbers, 4);
 
-		String[] someStrings = new String[1000];
-		standardPopulate(someStrings);
-		String s = someStrings[999];
-		makeSpecial(s);
-		print(someStrings);
+		
 		long endTime = System.currentTimeMillis();
 		System.out.println("The process took " + (endTime - currentTime) + " ms.");
 		
 		
 	}
 	
+	private static void countResult(int[] array, int numberOfDice) {
+		System.out.println("Count Result");
+		int[] counter = new int[numberOfDice*6];
+		for(int n: array){
+			counter[n-1] = counter[n - 1] + 1;
+		}
+		for(int i = numberOfDice - 1; i < counter.length; i ++){
+			System.out.println((i+1) + " was rolled " + 100*counter[i]/array.length + " percent of the time");
+		}
+	}
+
+	private static void rollDice(int[] array, int numberOfDice) {
+		System.out.println("Roll Dice");
+		for(int i = 0; i < array.length; i++){
+			int dice = 0;
+			for(int j = 0; j < numberOfDice; j ++){
+				dice = (int)(dice + (1+6*Math.random()));
+			}
+			array[i] = dice;
+		}
+	}
+
+	private static void randomize(int[] array , int max) {
+		System.out.println("Randomize");
+		for(int i = 0; i < array.length; i++){
+			int random = (int)(Math.random()*max) + 1;
+			array[i] = random;
+		}
+		
+	}
+
+	private static void print(int[] array) {
+		for(int i = 0; i < array.length; i++){
+			System.out.println(array[i]);
+		}
+		
+	}
+
+	private static void populate(int[] array) {
+		System.out.println("Populate");
+		for(int i = 0; i < array.length; i++){
+			array[i] = i+1;
+		}
+		
+	}
+
+	private void demonstratePassByValue(){
+		String[] someStrings = new String[1000];
+		standardPopulate(someStrings);
+		String s = someStrings[999];
+		makeSpecial(s);
+		someStrings[999] = getASpecialString();
+		print(someStrings);
+	}
+	
+	private static String getASpecialString() {
+		return "THIS STRING IS SPECIAL!";
+	}
+
 	private static void makeSpecial(String s) {
 		s = "THIS STRING IS SPECIAL!";
 		
