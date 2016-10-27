8f7d86d3b82136d2dfc61d5f604bdf46cf66d2c0
hotfi <hotfi@192.168.1.2>
2016-10-26 23:44:51 -0400

    mvp goon

--- Victor Chen Nockles8/src/arrays/AdvancedArrayMethods.java
+++ Victor Chen Nockles8/src/arrays/AdvancedArrayMethods.java
@@ -1,11 +1,17 @@
 package arrays;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class AdvancedArrayMethods {
 	
 	public static void main(String[] args){
 		String[] array = {"a" , "b" + "c" + "d" + "e" + "f" + "g" + "h"};
+		int[] array1 = {1,0,3,4};
+		int[] array2 = {1,2,3,4,5};
 		//swap(array, 0, array.length - 1);
-		shuffle(array);
+		// shuffle(array);
+		System.out.println(longestSharedSequence(array1, array2 ));
 	}
 
 		
@@ -20,7 +26,7 @@ public class AdvancedArrayMethods {
 	public static int longestSharedSequence(int[] array1, int[] array2){
 		int max = 0;
 		int count = 0;
-		for(int seqStart = 0; seqStart < array1.length; seqStart++){
+/*		for(int seqStart = 0; seqStart < array1.length; seqStart++){
 			int seqEnd = seqStart;
 			int[] seq = getSequence(seqStart, seqEnd, array1);
 			if(checkSequence(seq, array2)){
@@ -32,7 +38,26 @@ public class AdvancedArrayMethods {
 			// reset count after every sequence has been checked
 			count = 0;
 		}
-		return max;
+*/		
+		int ctr = 0;
+        int longest = 0;
+        for(int i = 0; i < array1.length; i++){
+         for(int j = 0; j < array2.length; j++){
+          if((i + ctr) < array1.length){
+           if(array1[i+ctr] == array2[j]){
+            ctr++;
+           }
+           else{
+            ctr = 0;
+           }
+           if(ctr > longest){
+            longest = ctr;
+           }
+          }
+          
+         } 
+        }
+        return longest;
 	}
 	
 	// returns true if seq is found inside array2
@@ -41,7 +66,7 @@ public class AdvancedArrayMethods {
 	A:	for(int i = 0; i < arr.length; i++){
 			// j checks every element in seq
 	B:		for(int j = 0; j < seq.length; j++){
-				if(j + i < arr.length && seq[j] != arr[j+1]){
+				if(j + i < arr.length && seq[j] != arr[j+i]){
 					// breaks out of innermost for loop unless particular for loop is 
 					// specified (labels "A:") break A: break B:
 					break;
@@ -58,8 +83,14 @@ public class AdvancedArrayMethods {
 
 	// returns a sub-array containing the elements
 	// in array 1 from seqStart to seqEnd
-	private static int[] getSequence(int seqStart, int seqEnd, int[] array1) {
-		return null;
+	private static int[] getSequence(int seqStart, int seqEnd, int[] array) {
+		int[] sequence = new int[(seqEnd - seqStart) + 1];
+		int counter = 0;
+		for(int i = seqStart; i < seqEnd + 1; i++){
+			sequence[counter] = array[i];
+			counter ++;
+		}
+		return sequence;
 
 	}
 	private static void swap(Object[] array, int a, int b) {
