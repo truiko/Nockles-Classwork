79097455c52b400f2ac7f479ae135bfbf1525278
Student 8 <Student 8@10.8.33.152>
2016-10-28 13:04:09 -0400

    prime prime prime

--- Victor Chen Nockles8/src/arrays/ArraysPractice.java
+++ Victor Chen Nockles8/src/arrays/ArraysPractice.java
@@ -5,8 +5,10 @@ public class ArraysPractice {
 	static boolean[] boos3; 
 	
 	public static void main(String[] args) {
+		
+		listPrimes(120);
 		// how do you time a process
-		long currentTime = System.currentTimeMillis();
+/*		long currentTime = System.currentTimeMillis();
 		int[] fiftyNumbers = new int[50];
 		populate(fiftyNumbers);
 		print(fiftyNumbers);
@@ -21,8 +23,41 @@ public class ArraysPractice {
 		long endTime = System.currentTimeMillis();
 		System.out.println("The process took " + (endTime - currentTime) + " ms.");
 		
-		
+*/
 	}
+	
+	private static void listPrimes(int limit) {
+		int lastToCheck = (int) (Math.sqrt(limit));
+		boolean[] numbers = new boolean[limit + 1];
+		for(int i = 0; i < limit + 1; i++){
+			numbers[i] = true;
+		}
+		
+		// 0 and 1 are, by definition, not prime
+		numbers[0] = false;
+		numbers[1] = false;
+		
+		// check all non-"crossed of" numbers (start with 2)
+		for(int prime = 2; prime <= lastToCheck; prime++){
+			if(numbers[prime]){
+				System.out.println("\n" + prime + " is prime." + " Crossing off:");
+				for(int i = (int)(Math.pow(prime,2)); i < limit+1; i += prime){
+					System.out.print(i + ", ");
+					numbers[i] = false;
+				}
+			}
+		}
+		// print the primes
+		System.out.print("\n The primes are:");
+		for(int index = 0; index < numbers.length; index++){
+			if(numbers[index]){
+				System.out.print(index + ", ");
+			}
+			
+		}
+	}
+
+	
 	
 	private static void countResult(int[] array, int numberOfDice) {
 		System.out.println("Count Result");
