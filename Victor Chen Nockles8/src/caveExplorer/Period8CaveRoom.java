c25df65da0c1c741133171c71e054f9181c92798
Student 8 <Student 8@BT_5E7_18>
2016-10-31 14:02:43 -0400

    caves man caves - cave man

---
+++ Victor Chen Nockles8/src/caveExplorer/Period8CaveRoom.java
@@ -0,0 +1,19 @@
+package caveExplorer;
+
+public class Period8CaveRoom {
+
+	public Period8CaveRoom(String description) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void enter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setConnection(Period8CaveRoom period8CaveRoom) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
