b5e99a3cbaeae547e2b416f219b8c35be88103c0
Student 8 <Student 8@10.8.33.152>
2016-11-01 12:59:54 -0400

    help

--- Victor Chen Nockles8/src/caveExplorer/CaveExplorer.java
+++ Victor Chen Nockles8/src/caveExplorer/CaveExplorer.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class CaveExplorer {
 
-	public static Period8CaveRoom[][] caves;
+	public static CaveRoomPd8[][] caves;
 	public static Scanner in ;
-	public static Period8CaveRoom currentRoom;
+	public static CaveRoomPd8 currentRoom;
 	public static InventoryNockles inventory;
 	
 	public static void main(String[] args){
 		in = new Scanner(System.in);
-		caves = new Period8CaveRoom [5][5];
+		caves = new CaveRoomPd8 [5][5];
 		for(int row = 0; row < caves.length; row++){
 			for(int col = 0; col < caves[row].length; col++){
-				caves[row][col] = new Period8CaveRoom("This room has coordinates" + 
+				caves[row][col] = new CaveRoomPd8("This room has coordinates" + 
 			row + "," + col);
 			}
 		}
