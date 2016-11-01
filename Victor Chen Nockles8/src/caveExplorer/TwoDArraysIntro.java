ba5ab464772220a7fb4e57ce3f033e31441259ea
Student 8 <Student 8@10.8.33.152>
2016-11-01 14:04:14 -0400

    birds man

--- Victor Chen Nockles8/src/caveExplorer/TwoDArraysIntro.java
+++ Victor Chen Nockles8/src/caveExplorer/TwoDArraysIntro.java
@@ -4,7 +4,43 @@ import java.util.Arrays;
 
 public class TwoDArraysIntro {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		String[][] pic = new String[10][20];
+		for(int row = 0; row < pic.length; row++){
+			// populate with coordinates
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = " ";
+			}
+		}
+		// sun
+		pic[0][3] = "|";
+		pic[1][3] = "O";
+		pic[2][3] = "|";
+		pic[1][2] = "-";
+		pic[1][4] = "-";
+		
+		// birds
+		pic[1][11] = "(";
+		pic[1][12] = "*";
+		pic[1][13] = ">";
+		pic[1][14] = "*";
+		pic[1][15] = ")";
+		// grassy field
+		for(int row = 5; row < pic.length; row++){
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = "M";
+			}
+		}
+		
+		for(String[] row: pic){
+			for(String col: row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}		
+	}
+	
+	public static void intro(){
 		String[] xox = {"x","o","x","o","x"};
 		System.out.println(Arrays.toString(xox));
 		// a 1D array prints a horizontal string
@@ -25,6 +61,7 @@ public class TwoDArraysIntro {
 		for(String[] row: arr2D){
 			System.out.println(Arrays.toString(row));
 		}
+		
 	}
 
 }
