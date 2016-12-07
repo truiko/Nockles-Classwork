158e2906453a27c653dbb29adf5cd73645ed9455
Student 8 <Student 8@10.8.33.152>
2016-12-07 14:03:02 -0500

    more gui

--- Victor Chen Nockles8/src/guiPractice/GUIApplication.java
+++ Victor Chen Nockles8/src/guiPractice/GUIApplication.java
@@ -1,14 +1,40 @@
 package guiPractice;
 
-public class GUIApplication {
+import java.awt.Graphics;
 
+import javax.swing.JFrame;
+
+public class GUIApplication extends JFrame{
+	
+	private Screen currentScreen;
+	
+	
+	// main method for practice only
+	public static void main(String[] args) {
+		new GUIApplication();
+
+	}
+	
 	public GUIApplication() {
-		// TODO Auto-generated constructor stub
+		// terminate program when window is close
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		int x = 40;
+		int y = 40;
+		int width = 600;
+		int height = 400;
+		setBounds(x,y,width,height);
+		initScreen();
+		setVisible(true);
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	protected void initScreen() {
+		Screen startScreen = new Screen(getWidth(), getHeight());
+		currentScreen = startScreen;
+		
+	}
 
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0 , 0, null);
 	}
 
 }
